1919a2b9b06ce3d70a7c998a3344aecb564fb0e3
paolo <paolosecci98@gmail.com>
2018-11-28 19:54:47 -0800

    fixed api

--- server2.py
+++ server2.py
@@ -7,24 +7,18 @@ def index():
     return render_template("index.html")
 
 @app.route("/data")
-def get_nba_gamelog():
+def get_data():
     import requests
-    USER_AGENT = (
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) ' +
-        'AppleWebKit/537.36 (KHTML, like Gecko) ' +
-        'Chrome/61.0.3163.100 Safari/537.36'
-    )
-    REQUEST_HEADERS = {
-        'user-agent': USER_AGENT,
-    }
-    
-    
-    url = "https://stats.nba.com/stats/leaguegamelog?Counter=1000&DateFrom=&DateTo=&Direction=DESC&LeagueID=00&PlayerOrTeam=P&Season=2018-19&SeasonType=Regular+Season&Sorter=DATE"
-    
-    re = requests.get(url, headers=REQUEST_HEADERS, allow_redirects=False, timeout=15)
+
+    nba_url = "https://stats.nba.com/stats/leaguegamelog?Counter=1000&DateFrom=&DateTo=&Direction=DESC&LeagueID=00&PlayerOrTeam=P&Season=2018-19&SeasonType=Regular+Season&Sorter=DATE"
+    this_user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+
+    re = requests.get(nba_url, headers=this_user_agent)
     print(re.status_code)
     
-    nba_data = re.json()
+    import json
+    nba_json = json.loads(re.text)
+
     return nba_json
 
 if __name__ == "__main__":
