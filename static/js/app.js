91d1b83569e5f8be1a1fe8332ba8955ddfa03715
Tiffany Chien <tiffanychien@Tiffanys-MacBook-Pro-2.local>
2018-11-29 23:33:25 -0800

    html

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,58 @@
 function playerStats(team) {
-  d3.json(`/<team_abbr>/players`).then((data) => {
-    // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(`/${team}/players`).then((data) => {
+    // Use d3 to select the panel with id of `#player-data`
     var PANEL = d3.select("#player-data");
 
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
     Object.entries(data).forEach((player) => {
       PANEL.append("h6").text(`${player}`);
     });
+  });
+}
 
+function playerStats2(team) {
+  d3.json(`/${team}/players`).then((data) => {
+    // Use d3 to select the panel with id of `#player-data`
+    var PANEL2 = d3.select("#player-data2")
+
+    // Use `.html("") to clear any existing metadata
+    PANEL2.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(data).forEach((player) => {
+      PANEL2.append("h6").text(`${player}`);
+    });
   });
 }
 
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-
+  var selectorFirst = d3.select("#selDataset");
+  var selectorSecond = d3.select("#selDataset2");
+  
   // Use the list of team abbr to populate the select options
   d3.json("/get_teams").then((teams) => {
     teams.forEach((team) => {
-      selector
+      selectorFirst
+        .append("option")
+        .text(team)
+        .property("value", team);
+      selectorSecond
         .append("option")
         .text(team)
         .property("value", team);
     });
-
-    const firstTeam = teams[0];
-    buildMetadata(firstTeam);
   });
 }
 
 function optionChanged(newTeam) {
-
   playerStats(newTeam);
+}
+function optionChanged2(newTeam2) {
+  playerStats2(newTeam2);
 }
 
 init();
